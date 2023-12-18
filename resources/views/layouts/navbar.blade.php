8652e45561d5f01be8b7b69ec67295221860b511
hirgy <hirgy9@gmail.com>
2023-12-18 15:17:54 +0700

    push dlu

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand cold-md-3 col-lg-2 me-0 px-3 fs-6" href="#">REKOMENDASI POSISI PEMAIN</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+
+          <div class="nav-item text-nowrap">
+            <li>
+              <form action="">
+                  <button type="submit" class="nav-link bg-primary-subtle text-emphasis-primary border-0"> Logout </button>
+              </form>
+            </li>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
